d43429ff040e13eb3bfe1e483247e732bf541dbb
EssenceOfNull <tomaskrul@gmail.com>
2023-05-10 16:32:56 +0300

    PortalBetweenLevels V0.5
    Additions:
    -Added a portal that makes the scene transition between levels based on scene build index

---
+++ ProjectP/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    public float teleportDelay=.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.isTrigger)
+        {
+            return;
+        }
+
+		if (collision.tag == "Player")
+		{
+            StartCoroutine(DelayedTeleport());
+        }
+    }
+    private IEnumerator DelayedTeleport()
+    {
+        yield return new WaitForSeconds(teleportDelay);
+
+        yield return new WaitForSeconds(0.05f);
+        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+    }
+}
